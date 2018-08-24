b1aa4b495147e4c1a4fa2c85977af0da85ec6cf0
Christian Davel Altan Garrido <CDAltan@Desarrollo.administracion.local>
2018-08-24 15:43:54 -0600

    12

--- src/app/github-profile/github-profile.component.ts
+++ src/app/github-profile/github-profile.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-github-profile',
@@ -8,7 +8,9 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class GithubProfileComponent implements OnInit {
 
-  constructor(private route: ActivatedRoute ) { }
+  constructor(
+    private router: Router,
+    private route: ActivatedRoute ) { }
 
 
   ngOnInit() {  
@@ -23,6 +25,12 @@ export class GithubProfileComponent implements OnInit {
          let id= +params.get('primerParametro');
          console.log(id);
       });
+  }
+
+  submit(){
+    this.router.navigate(['/followers'], {
+      queryParams: { page:1, order: 'newest' }
+    });
   }
 
 }
