90c061c2c8a4a1745f147a7fac9e687a3aeb904d
Christian Davel Altan Garrido <CDAltan@Desarrollo.administracion.local>
2018-08-24 09:39:05 -0600

    c2

--- src/app/github-profile/github-profile.component.ts
+++ src/app/github-profile/github-profile.component.ts
@@ -10,11 +10,16 @@ export class GithubProfileComponent implements OnInit {
 
   constructor(private route: ActivatedRoute ) { }
 
-  ngOnInit() {
-    this.route.paramMap
+
+  ngOnInit() {  
+     //this is if you navigate away from the component follower and then re load the component 
+     let id2 = this.route.snapshot.paramMap.get('id');
+      console.log(id2);
+
+      //this is when we stay on the same follower component but we switch only de id parameter
+       this.route.paramMap
       .subscribe(params =>{
-         let id= +params.get('id');               
-         //servie.getprofiledata(id);
+         let id= +params.get('id');
          console.log(id);
       });
   }
